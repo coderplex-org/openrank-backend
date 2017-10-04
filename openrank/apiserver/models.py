61c2df8acca295daf6a5694f33b88856cb0173e7
Om Anirudh <omanirudh2014@gmail.com>
2017-10-05 00:57:06 +0530

    "Totally Revamped. No syntaxt errors. Given Default max_length as 100 to all CharField() Defined the classes in proper order Except for the following 2 errors there are None.
    Fix these two and we will have our models Working.
    
    1)fields.E300 error on Relation b/w  Question.tags to Tags and TestCase.testcaseBatch_id to TestCaseBatch
    2) fields.E336 error on intermeddiate model not having ForeignKey to either Tag or Question
     issue is with Tag.questions "

--- openrank/apiserver/models.py
+++ openrank/apiserver/models.py
@@ -1,126 +1,84 @@
 from django.db import models
 
-# Create your models here.
-#All the Comments and doc-strings may contain Opinionated Views.
-#These are not the final docs. Constructive Negation is welcome!  ex  you may find the comments too-verbose
+class Role(models.Model):
+    role = models.CharField(max_length=100)  
+
+class User(models.Model):
+    role = models.OneToOneField(Role)  
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length =100)
+    password = models.CharField(max_length =100)
+    phone_number = models.CharField(max_length =100)
+
+class Position(models.Model):
+    job_title = models.CharField(max_length = 100)
 
 class Contest(models.Model):
-    name=models.CharField()
+    name=models.CharField(max_length=100)
     description=models.TextField()
     url = models.URLField()
     is_public = models.BooleanField()
     starttime = models.TimeField()
     user_id = models.ForeignKey(User)
-    position = models.ForeignKey(Position)
+    position = models.ForeignKey(Position)    
     cutoff_duration = models.IntegerField()
     login_start_time = models.TimeField()
-    password = models.charField(max_length=45)
+    password = models.CharField(max_length=100)  
 
-
-class Position(models.models):
-	job_title = models.charField()
-
-
-
-
-
-class User(models.Model):
-	'''Currently these fiels are planned
-	1)name -eh just the name.     should there be a max length? for name? 
-	2)email - email of the contestant.  BuiltIn  EmailField from django is being used.
-	3)password-
-	4)phone_number- an IntegerField.    how will we validate this? is there any Buildin  (like how it is for Email)
-
-'''
-
-    role = models.OneToOneField()  # A Role can oprionally be assigned to a User. But a User MUST be assigned to SOME ROLE
-	name  = models.CharField(max_length=60)
-	email = models.EmailField(dmax_length=60)
-	password = models.CharField(max_length=50)
-	phone_number = models.charField(max_length=10)
-
-
-class Enrollment(models.Model)
-    user_id =    models.ForeignKey(User)
+class Enrollment(models.Model):
+    user_id = models.ForeignKey(User)
     contest_id = models.ForeignKey(Contest,on_delete=models.CASCADE)
     marks_obtained = models.IntegerField()
-    user_details = models.charField()
-
-class Role(models.Model):
-	role = models.charField()
-
-
-class Question(models.Model):
-
-	description = models.TextField()
-	total_marks = models.IntegerField()
-	tags = models.manyToMany(Tag)
-
-	 
-
-
-class ContestQuestion(models.Model):
-	question_id = models.ForeignKey(Question,on_delete=models.CASCADE)
-	contest_id = models.ForeignKey(Contest,on_delete=models.CASCADE)
-	total_marks  = models.IntegerField()
-	time_limit   = models.IntegerField()
-	memory_limit = models.IntegerField()  
-	variable_constraints = models.CharField(max_length=45)
-
-
-class Language(models.Model):
-	language = models.charField()
-
-
-class QuestionLanguage(models.Model):
-	question_id = models.ForeignKey(Question,on_delete=models.CASCADE)
-	language_id = models.ForeignKey(Language,on_delete=models.CASCADE)
-	function_signature =  models.TextField()
-	function_parameters = models.charField()
-	starter_code =        models.charField()
-
+    user_details = models.CharField(max_length=100) #chesinaka already ni!!!
 
 class Tag(models.Model):
-	tag_description =   models.charField()  #A charField
-	questions = models.manyToMany(Question)
-    
+    tag_description =   models.CharField()     
+	
+class Question(models.Model):                      
+    description = models.TextField()            
+    total_marks = models.IntegerField()
+    tags = models.ManyToManyField(Tag)               
 
+class ContestQuestion(models.Model):
+    question_id = models.ForeignKey(Question,on_delete=models.CASCADE)
+    contest_id = models.ForeignKey(Contest,on_delete=models.CASCADE)
+    total_marks  = models.IntegerField()
+    time_limit   = models.IntegerField()
+    memory_limit = models.IntegerField()  
+    variable_constraints = models.CharField(max_length=100)
+
+class Language(models.Model):
+    language = models.CharField(max_length = 100)  
+
+class QuestionLanguage(models.Model):
+    question_id = models.ForeignKey(Question,on_delete=models.CASCADE)
+    language_id = models.ForeignKey(Language,on_delete=models.CASCADE)
+    function_signature =  models.TextField()
+    function_parameters = models.CharField(max_length=100)
+    starter_code = models.CharField(max_length=100)
+
+class Tag(models.Model):
+    tag_description = models.CharField(max_length=100)     
+    questions = models.ManyToManyField(Question)
+
+class TestCaseBatch(models.Model):
+    marks = models.IntegerField()
+    title = models.CharField()	
 class TestCase(models.Model):
-	testcase_input = models.charField()
-	output = models.charField()
-	testcaseBatch_id = models.ForeignKey(TestCaseBatch,on_delete=models.CASCADE)
-	question_id = models.ForeignKey(Question,on_delete = models.CASCADE)
-	is_hidden = models.BooleanField()
+    testcase_input = models.CharField(max_length=100)  
+    output = models.CharField(max_length=100)
+    testcaseBatch_id = models.ForeignKey(TestCaseBatch,on_delete=models.CASCADE) 
+    question_id = models.ForeignKey(Question,on_delete = models.CASCADE)
+    is_hidden = models.BooleanField()
 
 class TestCaseBatch(models.Model):
 	marks = models.IntegerField()
-	title = models.charField()
-
-
-
+	title = models.CharField(max_length=100)
 
 class Submission(models.Model):
-	''' Currently these fields are planned
-	Note  three stars before fieldName if it is a RelationshipField
-	1)***enrolment_id - will help identify which user submitted this sumission
-	             - It will relate to the Users model in a ManyToOne Relation (definately Not manyToMany because One  same submission can't be made by two distinct users )
-	
-	2)***contest_id - will help identify as part of which Contest this submission was made.
-	                  - It  will Relate to the Contest model
-
-	
-	3)scorevaluated -just an integer for now (atleast)  given by the scoringEngine for this sumission'
-
-	4)program   - a .py file  which is the code submited (Just For Now)
-	'''
-	#submitedby = 
-	#whichcontest= #Dropped instead this will be found from relation to Enrollment table
-	               #So there is no direct relation between Contest and Submission??  @Bhanu @Kapil
-	
-
 	score_valuated = models.IntegerField()
-	program  =  models.TextField()
+	program = models.TextField()
 	question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
 	enrollment_id = models.ForeignKey(Enrollment,on_delete=models.CASCADE)
-    language_id = models.ForeighKey(Language,on_delete=models.CASCADE)
+	language_id = models.ForeignKey(Language,on_delete=models.CASCADE)
 
