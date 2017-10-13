551367ec27a4e9941361b8f55dd462fcd129ce87
Buddha Jyothiprasad <jyothiprasadb@gmail.com>
2017-10-13 09:37:55 +0530

    restricting access control allow origin to be just frontend

--- openrank/apiserver/views.py
+++ openrank/apiserver/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+def prepareResponse(response):
+    # The front end URL has to eventually come from a configuration file to awoid
+    # hardcoding in source code
+    response["Access-Control-Allow-Origin"] = "http://localhost:8081"
+    return response
+
 # Create your views here.
 def helloworld(request):
-    response = HttpResponse("Hello world")
-    response["Access-Control-Allow-Origin"] = "*"
-    return response
+    response = HttpResponse("Hello from API Server")
+    return prepareResponse(response)
