c2a26f8dc14505ad4296034b46373f62cb1e1980
Buddha Jyothiprasad <jyothiprasadb@gmail.com>
2017-10-15 00:26:06 +0530

    code execution with input and output for multiple test cases

--- framework/Classes.py
+++ framework/Classes.py
@@ -1,6 +1,53 @@
+from enum import Enum
+
+
+class Status(Enum):
+    IN_PROGRESS = 0
+    OK = 1
+    COMPILE_ERROR = 2
+    RUNTIME_ERROR = 3
+    TIMEOUT = 3
+    ENV_CRASH = 4
+
+
+class ProgrammingLanguage(Enum):
+    C = "C"
+    CPP = "C++"
+    JAVA8 = "Java 8"
+    PYTHON2 = "Python 2"
+    PYTHON = "Python 3"
+    RUBY = "Ruby"
+    JAVASCRIPT = "Node.js"
+
+
+class Submission:
+    def __init__(self):
+        self.source = ""
+        self.programming_language = ""
+        self.compile_command = []
+        self.run_command = []
+        self.test_cases_input = []
+
+
+class Testcase:
+    def __init__(self):
+        self.id = ""
+        self.input = ""
+        self.file = ""
+        self.timeout = 2
+
+
 class Output:
     def __init__(self):
+        self.test_case_id = ""
+        self.memory = 0
+        self.time = 0
         self.output = ""
-        self.status = ""
+        self.status = Status.IN_PROGRESS
+
+    def __repr__(self):
+        return self.output
+
+
 
 
--- framework/runner.py
+++ framework/runner.py
@@ -3,41 +3,84 @@ import os
 import uuid
 import shutil
 
-from framework.Classes import Output
+from framework.Classes import Output, Status, Testcase
 
 
 def run(source, source_extension, compile_command, run_command, test_cases):
     source_file_name = "Source." + source_extension
-    output = Output()
+    result = []
     current_directory = os.getcwd()
     temp_dir = uuid.uuid4().hex
     os.makedirs(temp_dir)
     os.chdir(temp_dir)
-    text_file = open(source_file_name, "w")
-    text_file.write(source)
-    text_file.close()
 
-    if compile_command:
-        status = subprocess.call([compile_command, source_file_name])
-        if status:
-            output.status = "COMPILE_ERROR"
-            output.custom = "XYZ"
+    try:
+        text_file = open(source_file_name, "w")
+        text_file.write(source)
+        text_file.close()
 
-    if not output.status and run_command:
-        subprocess.call(run_command)
+        status = 0
+        out_compile = Output()
+        if compile_command:
+            completed = subprocess.run([compile_command, source_file_name],
+                                       stdout=subprocess.PIPE,
+                                       stderr=subprocess.PIPE)
+            if completed.returncode:
+                result.append(out_compile)
+                out_compile.status = Status.COMPILE_ERROR
+                out_compile.output = completed.stdout.decode('utf-8') + completed.stderr.decode('utf-8')
+
+        if not out_compile.status:
+            for test_case in test_cases:
+                out_test = Output()
+                out_test.test_case_id = test_case.id
+                result.append(out_test)
+                if run_command:
+                    completed = subprocess.run(run_command,
+                                               stdout=subprocess.PIPE,
+                                               stderr=subprocess.PIPE,
+                                               input=test_case.input.encode('utf-8'),
+                                               timeout=test_case.timeout)
+                    out_test.output = completed.stdout.decode('utf-8') + completed.stderr.decode('utf-8')
+                    out_test.output = out_test.output.rstrip()
+                    if completed.returncode:
+                        out_test.status = Status.RUNTIME_ERROR
+                    else:
+                        out_test.status = Status.OK
+
+    except:
+        out_exception = Output()
+        result.append(out_exception)
+        out_exception.status = Status.ENV_CRASH
+        print(os.sys.exc_info())
 
     os.chdir(current_directory)
     shutil.rmtree(temp_dir, True)
 
-    return output
+    return result
 
 
-source_code = """class Solution{
+source_code = """
+                import java.util.*;
+                
+                class Solution{
                     public static void main(String... args) {
-                        System.out.println("Hello Compiler");
+                        Scanner scan = new Scanner(System.in);
+                        int sum = scan.nextInt() + scan.nextInt();
+                        System.out.println(sum);
                     }
-                }"""
+                }
+             """
 
-out = run(source_code, "java", "javac", ["java","Solution"], [])
-print(out.output)
-print(out.status)
+tc1 = Testcase()
+tc1.id = "1"
+tc1.input = "23 34"
+tc1.timeout = 1
+
+tc2 = Testcase()
+tc2.id = "2"
+tc2.input = "21 34"
+tc2.timeout = 1
+
+out = run(source_code, "java", "javac", ["java","Solution"], [tc1, tc2])
+print(out)
