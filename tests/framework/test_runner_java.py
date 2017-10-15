a54203c5493d89692dd5fab95ace1046d38574d1
Buddha Jyothiprasad <jyothiprasadb@gmail.com>
2017-10-15 19:29:07 +0530

    unit tests that are common to all programming languages and java specific

--- tests/framework/test_runner_java.py
+++ tests/framework/test_runner_java.py
@@ -1,6 +1,13 @@
 from framework.Classes import Status
 from framework.runner import run
-from data import java_source_code_with_exception, java_source_code_with_compile_error
+from data import java_source_code_with_exception, java_source_code_with_compile_error, java_source_code_with_input, tc1
+
+
+def test_add_two_numbers_code():
+    out = run(java_source_code_with_input, "java", "javac", ["java", "Solution"], [tc1])
+    assert 1 == len(out)
+    assert '57' == out[0].output
+    assert Status.OK == out[0].status
 
 
 def test_executable_java_code_with_runtime_exception():
