fbf9d581269bf45783eaaf4ad52f83ab66c935c8
Buddha Jyothiprasad <jyothiprasadb@gmail.com>
2017-10-16 09:25:44 +0530

    accounting for difference in compile errors in travis

---
+++ framework/test_runner_cpp.py
@@ -0,0 +1,18 @@
+from framework.Classes import Status
+from framework.runner import run
+from framework.test_data import tc1, c_source_code_add_two_numbers, c_source_code_add_two_numbers_compile_error
+
+
+def test_c_add_two_numbers_code():
+    out = run(c_source_code_add_two_numbers, "c", ["gcc", "-o", "program"], ["./program"], [tc1])
+    assert 1 == len(out)
+    assert '57' == out[0].stdout
+    assert Status.OK == out[0].status
+
+
+def test_c_add_two_numbers_code_compile_error():
+    out = run(c_source_code_add_two_numbers_compile_error, "c", ["gcc", "-o", "program"], ["./program"], [tc1])
+    print(out)
+    assert 1 == len(out)
+    assert "error: expected ';'" in out[0].stderr
+    assert Status.COMPILE_ERROR == out[0].status
