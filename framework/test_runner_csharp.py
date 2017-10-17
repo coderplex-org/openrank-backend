3bae1890d6390f4c0f0311f05ac4c71a68793c85
Buddha Jyothiprasad <jyothiprasadb@gmail.com>
2017-10-17 08:56:17 +0530

    travis cs fix attempt

--- framework/test_runner_csharp.py
+++ framework/test_runner_csharp.py
@@ -5,6 +5,7 @@ from framework.test_data import tc1, cs_source_code_add_two_numbers
 
 def test_cs_add_two_numbers_code():
     out = run(cs_source_code_add_two_numbers, "cs", ["mcs"], ["mono", "Program.exe"], [tc1])
+    print(out)
     assert 1 == len(out)
     assert '57' == out[0].stdout
     assert Status.OK == out[0].status
